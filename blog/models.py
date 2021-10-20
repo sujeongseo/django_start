2f7fec3e977572add06a54fd1509e51a02ec24e3
crystal <crystalseo.93@gmail.com>
2021-10-20 11:53:04 +0900

    reset db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=32)
+    content = models.TextField()
+    likes = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}, {self.content[:20]}"
